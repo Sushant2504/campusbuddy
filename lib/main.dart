c209b7845440b2cd02d38f24bc585f2f1a26c384
Sushant <sushantnadavade006@gmail.com>
2025-01-30 21:38:53 +0530

    files structure modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart'; // Now starts with SplashScreen
+import 'screens/splash_screen.dart'; // Now starts with SplashScreen
 
 void main() {
   runApp(MyApp());
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+
       home: SplashScreen(), // Starts with SplashScreen
     );
   }

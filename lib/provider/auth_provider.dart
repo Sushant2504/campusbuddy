4280ff3a73c9077740c16a7b102d8e256ef9bbd5
Sushant Nadavade <118672537+Sushant2504@users.noreply.github.com>
2025-01-31 13:31:04 +0530

    Update auth_provider.dart

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 Future<UserCredential> signInWithGoogle() async {
   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
